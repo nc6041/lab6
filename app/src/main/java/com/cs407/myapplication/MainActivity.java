ceecb6202860e6c4176f04d0382fb71da945e28f
Natalie Cheng <nataliec6041@gmail.com>
2023-11-02 23:23:37 -0500

    Initial Commit + some stuff

---
+++ app/src/main/java/com/cs407/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cs407.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+    }
+}
