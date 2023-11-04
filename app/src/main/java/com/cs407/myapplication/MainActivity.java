c9cea6227097770d0c73e328c3658258e45dd3b9
Natalie Cheng <nataliec6041@gmail.com>
2023-11-03 23:24:56 -0500

    milestone 1 done

--- app/src/main/java/com/cs407/myapplication/MainActivity.java
+++ app/src/main/java/com/cs407/myapplication/MainActivity.java
@@ -1,18 +1,29 @@
 package com.cs407.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final LatLng mDestinationLatLng = new LatLng(43.0757378, -89.4065813);
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
